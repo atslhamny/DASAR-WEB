539273610c02ccc4d4332f0125a685fe06f35ccb
Atsilah Amany <atsilahamanyph@gmail.com>
2024-09-18 13:56:07 +0700

    jbs04: add Praktikum Bagian 1. Variable dan Konstanta

---
+++ JBS04/variable_konstanta.php
@@ -0,0 +1,18 @@
+<?php 
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo " Hasil Penjumlahan $angka1 dan $angka2 adalah $hasil<br>";
+
+
+$benar = true;
+$salah = false;
+echo "Variable benar: $benar, Variable salah: $salah<br>";
+
+define("NAMA_SITUS", "Websiteku.com");
+define("TAHUN_PENDIRIAN", 2024);
+
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang
+didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
+
+?>
