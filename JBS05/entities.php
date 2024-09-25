d91f9c20e1e1d4b6c8304de9bfef913ed864099a
Atsilah Amany <atsilahamanyph@gmail.com>
2024-09-25 16:14:58 +0700

    JS05-Praktikum  9. Entitas HTML

---
+++ JBS05/entities.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Entities HTML</title>
+</head>
+
+<body>
+    <p>It&#39; time to read a HTML5 book.</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities:
+        sebuah nama entitas mudah diingat. kerugian dari menggunakan nama
+        entities: Browser mungkin tidak mendukung semua nama entitas, tetapi
+        dukungan untuk nomor lebih baik.
+    </p>
+    <p>&#169; 2024 jti.com</p>
+</body>
+
+</html>
