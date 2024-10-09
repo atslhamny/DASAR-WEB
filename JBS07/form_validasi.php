2ccfff9ad011e2f0144fc336d47c7e89069f19f5
Atsilah Amany <atsilahamanyph@gmail.com>
2024-10-09 15:34:12 +0700

    JS07-Praktikum 6 : Form Lanjut

---
+++ JBS07/form_validasi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input dengan Validasi</title>
+</head>
+<body>
+    <h1>Form Input dengan Validasi</h1>
+    <form action="proses_validasi.php" method="POST">
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama">
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+        <br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
