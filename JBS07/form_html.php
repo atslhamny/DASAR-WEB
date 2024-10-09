4bf445691b9964b4286be6e0b906208aa1ec6b72
Atsilah Amany <atsilahamanyph@gmail.com>
2024-10-09 13:50:23 +0700

    JS07-Praktikum 4 : HTML Injection

---
+++ JBS07/form_html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input</title>
+</head>
+
+<body>
+    <h2>Form Input Aman</h2>
+    <form action="html_aman.php" method="POST">
+        <label for="input">Masukkan Teks:</label>
+        <input type="text" id="input" name="input">
+        <button type="submit">Kirim</button>
+    </form>
+</body>
+
+</html>
