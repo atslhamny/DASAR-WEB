634aadb839401cd795c48ed244170fd410a32e1c
Atsilah Amany <atsilahamanyph@gmail.com>
2024-10-09 11:48:28 +0700

    JS07-Praktikum 2. Function empty

---
+++ JBS07/empty.php
@@ -0,0 +1,15 @@
+<?php
+$myArray = array(); // Array Kosng
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong";
+}else{
+    echo "Array terdefinisi dan tidak kosong";
+}
+echo "<br>";
+
+if(empty($nonExinstentVar)){
+    echo "Variable tidak terdefinisi atau kosong";
+}else{
+    echo "Variable terdefinisi dan tidak kosong";
+}
+?>
