1a103e4e307b853834becbb285a24c32d467ed99
Atsilah Amany <atsilahamanyph@gmail.com>
2024-10-16 12:09:18 +0700

    JS08-Praktikum Bagian 3. Upload File dengan PHP dan Jquery

---
+++ JBS08/form_upload_ajax.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Unggah File Dokumen</title>
+    </head>
+    <body>
+        <form action="upload_ajax.php" id="upload-form" method="POST" enctype="multipart/form-data">
+            <input type="file" name="files[]" id="file" multiple>
+            <input type="submit" name="submit" value="Unggah">
+        </form>
+        <div id="status"></div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="upload.js"></script>
+    </body>
+</html>
---
+++ JBS08/upload_ajax.php
@@ -0,0 +1,41 @@
+<?php
+if (isset($_FILES['files'])) {
+    $errors = array();
+    $allowedExtensions = array("png", "jpeg", "jpg", "gif");
+    $maxSize = 2 * 1024 * 1024; // Maksimal 2 MB per file
+    $fileCount = count($_FILES['files']['name']);
+
+    // Loop dengan for
+    for ($i = 0; $i < $fileCount; $i++) {
+        $file_name = $_FILES['files']['name'][$i];
+        $file_size = $_FILES['files']['size'][$i];
+        $file_tmp = $_FILES['files']['tmp_name'][$i];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+        // Validasi tipe file dan ukuran
+        if (!in_array($file_ext, $allowedExtensions)) {
+            $errors[] = "File {$file_name} memiliki ekstensi tidak valid. Hanya PNG, JPEG, JPG, dan GIF yang diperbolehkan.";
+        }
+
+        if ($file_size > $maxSize) {
+            $errors[] = "File {$file_name} melebihi ukuran maksimal 2 MB.";
+        }
+
+        // Jika tidak ada error, upload file
+        if (empty($errors)) {
+            $targetDirectory = "documents/";
+            if (move_uploaded_file($file_tmp, $targetDirectory . $file_name)) {
+                echo "File {$file_name} berhasil diunggah.<br>";
+                // Menampilkan thumbnail gambar
+                echo "<img src='documents/{$file_name}' width='200' style='height: auto;' alt='Thumbnail'><br>";
+            } else {
+                $errors[] = "Gagal mengunggah file {$file_name}.";
+            }
+        }
+    }
+
+    // Tampilkan semua error jika ada
+    if (!empty($errors)) {
+        echo implode("<br>", $errors);
+    }
+}
