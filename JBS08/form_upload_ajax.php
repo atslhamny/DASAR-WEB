d29f0a1fc63b36c446586a72dbae2703c0d9c7dd
Atsilah Amany <atsilahamanyph@gmail.com>
2024-10-16 14:06:56 +0700

    JS08-Praktikum Bagian 4. Menghias Upload File

--- JBS08/form_upload_ajax.php
+++ JBS08/form_upload_ajax.php
@@ -1,15 +1,26 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Unggah File Dokumen</title>
-    </head>
-    <body>
-        <form action="upload_ajax.php" id="upload-form" method="POST" enctype="multipart/form-data">
-            <input type="file" name="files[]" id="file" multiple>
-            <input type="submit" name="submit" value="Unggah">
-        </form>
-        <div id="status"></div>
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="upload.js"></script>
-    </body>
+<head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title>
+</head>
+
+<body>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        <form id="upload-form" action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="myFile" id="file" class="file-input"> <!-- Mengubah name menjadi 'myFile' -->
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+        </form>
+        <div id="status" class="upload-status"></div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</body>
+
 </html>
--- JBS08/upload.php
+++ JBS08/upload.php
@@ -1,21 +1,24 @@
-<?php 
-if (isset($_POST["submit"])) {
-    $targetdir = "uploads/"; //direktori tujuan untuk simpan file
+<?php
+if (isset($_FILES["myFile"])) {
+    $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myFile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxSize = 3*1024*1024;
 
-    if(in_array($fileType, $allowedExtensions) && $_FILES["myFile"]["size"]<=$maxSize){
+    // Logging untuk memeriksa file yang diterima di server
+    error_log("File diterima: " . $_FILES["myFile"]["name"]);
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myFile"]["size"] <= $maxSize) {
         if (move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah";
-            // echo "<img src='$targetfile' width='200' style='height: auto;' alt='Thumbnail'>";
         } else {
-            echo "gagal mengunggah file.";
+            echo "Gagal mengunggah file.";
         }
-    }else{
+    } else {
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
+} else {
+    echo "Tidak ada file yang diunggah.";
 }
-?>
