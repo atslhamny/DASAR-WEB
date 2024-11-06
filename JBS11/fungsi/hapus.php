885e8055ec0ae2c69a1a4571e6a59dc1a2b821c2
Atsilah Amany <atsilahamanyph@gmail.com>
2024-11-06 23:36:53 +0700

    JBS11-Praktikum 5. Folder Module – Bagian Anggota

--- JBS11/fungsi/hapus.php
+++ JBS11/fungsi/hapus.php
@@ -5,6 +5,7 @@ if (!empty($_SESSION['username'])) {
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
 
+    // Check if `jabatan` is being deleted
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
@@ -16,5 +17,28 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ../index.php?page=jabatan");
+        exit();
+    }
+    // Check if `anggota` is being deleted
+    elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+
+        // First delete from `anggota` table
+        $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+        if (mysqli_query($koneksi, $query2)) {
+
+            // If successful, delete from `user` table
+            $query2 = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query2)) { // Corrected `$query` instead of `$query2`
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Anggota Terhapus Tetapi Data Login Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Gagal Menghapus Data Anggota Karena: " . mysqli_error($koneksi));
+        }
+
+        header("Location: ../index.php?page=anggota");
+        exit();
     }
 }
