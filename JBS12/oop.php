1c3ca0e5dcf5d17522d5536266502fa9674b2b05
Atsilah Amany <atsilahamanyph@gmail.com>
2024-11-13 13:45:43 +0700

    JBS12-Praktikum 1. Basic OOP Soal 1.3

--- JBS12/oop.php
+++ JBS12/oop.php
@@ -64,47 +64,47 @@ $dog->sleep();
 $cat->meow();
 $dog->bark();
 
-// // 1.3
-// interface Shape
-// {
-//     public function calculateArea();
-// }
-// class Circle implements Shape
-// {
-//     private $radius;
+// 1.3
+interface Shape
+{
+    public function calculateArea();
+}
+class Circle implements Shape
+{
+    private $radius;
 
-//     public function __construct($radius)
-//     {
-//         $this->radius = $radius;
-//     }
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
 
-//     public function calculateArea()
-//     {
-//         return pi() * pow($this->radius, 2);
-//     }
-// }
+    public function calculateArea()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
 
-// class Rectangle implements Shape
-// {
-//     private $width;
-//     private $height;
+class Rectangle implements Shape
+{
+    private $width;
+    private $height;
 
-//     public function calculateArea()
-//     {
-//         return $this->width * $this->height;
-//     }
-// }
+    public function calculateArea()
+    {
+        return $this->width * $this->height;
+    }
+}
 
-// function printArea(Shape $shape)
-// {
-//     echo "Area: " . $shape->calculateArea() . "<br>";
-// }
+function printArea(Shape $shape)
+{
+    echo "Area: " . $shape->calculateArea() . "<br>";
+}
 
-// $circle = new Circle(5);
-// $rectangle = new Rectangle(4, 5);
+$circle = new Circle(5);
+$rectangle = new Rectangle(4, 5);
 
-// printArea($circle);
-// printArea($rectangle);
+printArea($circle);
+printArea($rectangle);
 
 // // 1.4
 // class Car
