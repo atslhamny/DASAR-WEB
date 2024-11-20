a7d06ae8633242dd98f7b0fa7fd272faa8fcad83
Atsilah Amany <atsilahamanyph@gmail.com>
2024-11-20 11:26:55 +0700

    JBS13-Praktikum 3 - Penerapan Layouting

--- JBS13/index.php
+++ JBS13/index.php
@@ -11,14 +11,20 @@ if ($session->get('is_login') !== true) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>DASHBOARD ATSILAH</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="adminlte/plugins/datatablesbs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="adminlte/plugins/datatablesresponsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="adminlte/plugins/datatablesbuttons/css/buttons.bootstrap4.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  <!-- jQuery -->
+  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -32,8 +38,8 @@ if ($session->get('is_login') !== true) {
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="adminlte/index3.html" class="brand-link">
-        <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="images.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">SIAKAD POLINEMA</span>
       </a>
 
       <!-- Sidebar -->
@@ -78,10 +84,26 @@ if ($session->get('is_login') !== true) {
   </div>
   <!-- ./wrapper -->
 
-  <!-- jQuery -->
-  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
+
   <!-- Bootstrap 4 -->
   <script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- jQuery Validation -->
+  <script src="adminlte/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="adminlte/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="adminlte/plugins/jquery-validation/localization/messages_id.min.js"></script>
+  <!-- DataTables & Plugins -->
+  <script src="adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/datatablesresponsive/js/dataTables.responsive.min.js"></script>
+  <script src="adminlte/plugins/datatablesresponsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/jszip/jszip.min.js"></script>
+  <script src="adminlte/plugins/pdfmake/pdfmake.min.js"></script>
+  <script src="adminlte/plugins/pdfmake/vfs_fonts.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App -->
   <script src="adminlte/dist/js/adminlte.min.js"></script>
   <!-- AdminLTE for demo purposes -->
